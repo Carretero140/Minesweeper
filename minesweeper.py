bb2845c0aca9a99d34b584725c07f3c2a99dabe8
carretero <jose.carretero@upb.edu.co>
2018-09-05 17:17:42 -0500

    leer las entradas

--- minesweeper.py
+++ minesweeper.py
@@ -1,1 +1,5 @@
-print("Hello Minesweeper")
+
+filas = input("Entre el numero de filas del tablero: ")
+columnas = input("Entre el numero de columnas del tablero: ")
+minas = input("Entre el numero de minas: ")
+print(filas,columnas,minas)
