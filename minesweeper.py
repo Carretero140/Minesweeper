1c73a952670d2ed3089178bc893354620d4d1f2c
carretero <jose.carretero@upb.edu.co>
2018-09-05 17:39:00 -0500

    create an empty grid for the mines

--- minesweeper.py
+++ minesweeper.py
@@ -1,5 +1,13 @@
+import numpy as np
+import random
+##
+filas = int(input("Entre el numero de filas del tablero: "))
+columnas = int(input("Entre el numero de columnas del tablero: "))
+minas = int(input("Entre el numero de minas: "))
 
-filas = input("Entre el numero de filas del tablero: ")
-columnas = input("Entre el numero de columnas del tablero: ")
-minas = input("Entre el numero de minas: ")
-print(filas,columnas,minas)
+def armar_grid(filas,columnas,minas):
+    grid_minas = np.zeros(shape=(filas,columnas))
+    print(grid_minas)
+
+armar_grid(filas,columnas,minas)
+    
