ebad5f9dded8d38fe277cff07bae79b2d29eaf14
carretero <jose.carretero@upb.edu.co>
2018-09-06 17:22:10 -0500

    metodo seleccionar, destapar y contar adyacentes implementados

--- minesweeper.py
+++ minesweeper.py
@@ -8,8 +8,12 @@ class Minesweeper:
         self.filas = int(input("Entre el numero de filas del tablero: "))
         self.columnas = int(input("Entre el numero de columnas del tablero: "))
         self.minas = int(input("Entre el numero de minas: "))
-        self.armar_grid(self.filas,self.columnas,self.minas)
+        self.grid = self.armar_grid(self.filas,self.columnas,self.minas)
+        self.tablero = self.armar_tablero(self.grid)
 
+    def __del__(self):
+        print("Perdio el juego")
+    
     def armar_grid(self,filas,columnas,minas):
         grid_minas = np.zeros(shape=(filas,columnas))
         random.seed()
@@ -17,7 +21,59 @@ class Minesweeper:
         #print(pos_minas)
         for el in pos_minas:
             grid_minas[el[0]][el[1]] = 1
-        print(grid_minas)
+        #print(grid_minas)
 
+    def armar_tablero(self,grid):
+        tablero = grid
+        tablero[tablero >= 0] = '.'
+        return tablero
+
+    def seleccionar_celda(self):
+        x = int(input("Selecciona la fila de la celda: "))
+        y = int(input("Selecciona la columna de la celda: "))
+        accion = input("Selecciona la accion a tomar: ").upper()
+
+        if accion == 'M':
+            self.marcar_celda(x,y)
+        elif accion == 'U':
+            self.destapar_celda(x,y)
+        else:
+            print("Entre M para marcar o U para Destapar")
+
+    def destapar_celda(self,x,y):
+        if self.grid[x][y] == 1:
+            del buscaminas
+        else:
+            minas_adyacentes = self.contar_adyacentes(x,y)
+            if minas_adyacentes == 0:
+                for el in matriz_adyacencia:
+                    self.tablero[el[0]][el[1]] = '_' 
+            else:
+                self.tablero[x][y] = minas_adyacentes
     
+    def contar_adyacentes(self,x,y):
+        bombas = 0
+        if self.grid[x-1][y] == 1:
+            bombas += 1
+        elif self.grid[x][y-1] == 1:
+            bombas += 1
+        elif self.grid[x-1][y-1] == 1:
+            bombas +=1
+        elif self.grid[x+1][y] == 1:
+            bombas +=1
+        elif self.grid[x][y+1] == 1:
+            bombas +=1
+        elif self.grid[x+1][y+1] == 1:
+            bombas +=1
+        elif self.grid[x+1][y-1] == 1:
+            bombas +=1
+        elif self.grid[x-1][y+1] == 1:
+            bombas +=1
+        
+        return bombas
+
+    def marcar_celda(self,x,y):
+        print('hola')
+
+
 buscaminas = Minesweeper()
