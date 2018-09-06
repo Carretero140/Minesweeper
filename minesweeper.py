825353751c6098433894d991c1a968d1326f5081
carretero <jose.carretero@upb.edu.co>
2018-09-06 14:49:38 -0500

    added init function and made it into a class for OOP

--- minesweeper.py
+++ minesweeper.py
@@ -1,13 +1,18 @@
 import numpy as np
 import random
 ##
-filas = int(input("Entre el numero de filas del tablero: "))
-columnas = int(input("Entre el numero de columnas del tablero: "))
-minas = int(input("Entre el numero de minas: "))
 
-def armar_grid(filas,columnas,minas):
-    grid_minas = np.zeros(shape=(filas,columnas))
-    print(grid_minas)
+class Minesweeper:
 
-armar_grid(filas,columnas,minas)
+    def __init__(self):
+        self.filas = int(input("Entre el numero de filas del tablero: "))
+        self.columnas = int(input("Entre el numero de columnas del tablero: "))
+        self.minas = int(input("Entre el numero de minas: "))
+        self.armar_grid(self.filas,self.columnas,self.minas)
+
+    def armar_grid(self,filas,columnas,minas):
+        grid_minas = np.zeros(shape=(filas,columnas))
+        print(grid_minas)
+
     
+buscaminas = Minesweeper()
